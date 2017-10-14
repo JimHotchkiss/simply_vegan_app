81905e9fff6d7080d21d2b514289f1be3d4c0d5c
jimhotchkiss <jimbohtochkiss@yahoo.com>
2017-10-14 17:37:46 -0400

    Add user registration in controller and form. Add static#welcome view and controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    find_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+
 end
